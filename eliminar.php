1c059845e7d5b75e9caaa41c77ec7e1fca22a7f9
Dafnne <dafnemarelyr@gmail.com>
2026-04-14 01:29:44 -0700

    Update eliminar.php

--- eliminar.php
+++ eliminar.php
@@ -1,5 +1,18 @@
 <?php
 include("conexion.php");
-$conn->query("DELETE FROM usuarios WHERE id=$_GET[id]");
+
+if(isset($_GET['id'])){
+
+    $id = $_GET['id'];
+
+    // Preparar consulta segura
+    $stmt = $conn->prepare("DELETE FROM usuarios WHERE id=?");
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+
+}
+
+// Redirigir
 header("Location: panel.php");
+exit;
 ?>
