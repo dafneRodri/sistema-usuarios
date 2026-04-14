7ba681126c02f2c6966add7c9925cc490a933d90
Dafnne <dafnemarelyr@gmail.com>
2026-04-13 21:22:17 -0700

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,6 +1,22 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "app_db");
+
+// Mostrar errores
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Conexión a la base de datos
+$host = "localhost";
+$user = "appuser";
+$pass = "1234";
+$db   = "app_db";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
 if ($conn->connect_error) {
-    die("Error conexión: " . $conn->connect_error);
+    die("Error de conexión: " . $conn->connect_error);
 }
+
+$conn->set_charset("utf8");
+
 ?>
